d1d36f8b3c18007689c9cb791ff55323a33c2dce
aissa-laribi <aissalaribi@yahoo.fr>
2022-04-09 13:21:58 +0100

    Defining explicitely attributes from cmagick.py in main.py

--- src/cmagick/main.py
+++ src/cmagick/main.py
@@ -2,6 +2,9 @@ def main():
     
     from cmagick import convert, resize
 
+    sourcefile = sourcefile
+    destinationfile = destinationfile
+    size = size
     convert = convert.convert(sourcefile, destinationfile)
     resize = resize.convert(sourcefile,size, destinationfile)
     
