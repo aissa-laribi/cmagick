c72f3ced166d0e6860032dfad62f6f20e5e0a6b0
aissa-laribi <aissalaribi@yahoo.fr>
2022-04-10 13:47:24 +0100

    Adding the wheel for v.0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmagick',
-    version='0.0.1',
+    version='0.0.5',
     author='Aissa Laribi',
     author_email='aissalaribi@yahoo.fr',
     description= 'An utility for converting and resizing images',
