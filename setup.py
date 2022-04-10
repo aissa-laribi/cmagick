034c83bc1c28d05370bd97076fc6b179f0ba51dc
aissa-laribi <aissalaribi@yahoo.fr>
2022-04-10 22:08:47 +0100

    First pip release

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     author='Aissa Laribi',
     author_email='aissalaribi@yahoo.fr',
     description='An utility for converting and resizing images',
+    long_description='README.md',
+    long_description_content_type='text/x-rst',
     url='https://github.com/aissa-laribi/cmagick',
     packages=find_packages('src'),
     package_dir={'': 'src'},
